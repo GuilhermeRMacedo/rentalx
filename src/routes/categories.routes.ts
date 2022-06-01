7f4c55a3507c683a1b9ef8f6da4d4b8fa9151bb9
Guilherme Macedo <guilhermericardord@gmail.com>
2022-06-01 00:20:42 -0300

    add multer dependency to work with files import and implementing simple import service

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer, { Multer } from 'multer';
 
 import { CategoryController } from '../controller/CategoryController';
 import { CategoryRepository } from '../repository/CategoryRepository';
 import { CategoryService } from '../service/CategoryService';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 const categoryRepository = new CategoryRepository();
 const categoryService = new CategoryService(categoryRepository);
@@ -16,6 +21,12 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
   return categoryController.readAll(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
 });
 
 export { categoriesRoutes };
